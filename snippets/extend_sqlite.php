80555203da308571dccb11feb37ee1fb7fd57787
Glenn Plas <glenn@byte-consult.be>
2012-01-13 13:25:18 +0100

    Added extending example

---
+++ snippets/extend_sqlite.php
@@ -0,0 +1,18 @@
+<?php
+class BaseClass {
+   function __construct() {
+       print "In BaseClass constructor\n";
+   }
+}
+
+class SubClass extends BaseClass {
+   function __construct() {
+       parent::__construct();
+       print "In SubClass constructor\n";
+   }
+}
+
+$obj = new BaseClass();
+$obj = new SubClass();
+?>
+
